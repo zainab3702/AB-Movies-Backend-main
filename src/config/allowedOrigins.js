2e6c337570308e5dc0a10bb3bcfecc71d82a1a84
zainab3702 <zainabfaiqua03@gmail.com>
2024-04-25 23:55:57 +0530

    first commit

--- src/config/allowedOrigins.js
+++ src/config/allowedOrigins.js
@@ -2,7 +2,7 @@
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://ab-movies-backend-7r0c.onrender.com",
+  "https://ab-movies-backend-8uu1.onrender.com",
   "https://ab-movies-frontend-2cta.onrender.com"
 ];
 
